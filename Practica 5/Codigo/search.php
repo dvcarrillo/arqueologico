6dd2c0c312754d8effdcbfd81c6a6b7fd4ea3d05
David Vargas <dv.carrillo@yahoo.com>
2018-06-05 19:29:27 +0200

    Modified style and behaviour of the search bar

--- Practica 5/Codigo/search.php
+++ Practica 5/Codigo/search.php
@@ -5,6 +5,7 @@ class result{
 	public $titulo;
 	public $subtitulo;
 	public $imagen_principal;
+
 	public function __construct($titulo, $subtitulo, $imagen_principal){
 		$this->titulo = $titulo;
 		$this->subtitulo = $subtitulo;
@@ -15,14 +16,14 @@ class result{
 function articleSearch($input){
 	$list = [];
 	$db = ConexionDB::getInstance();
-	$result = $db->query("SELECT * FROM articulos WHERE (`titulo` LIKE '%".$input."%') OR (`subtitulo` LIKE '%".$input."%')");
+	$result = $db->query("SELECT * FROM articulos WHERE (`titulo` LIKE '%".$input."%') OR 
+	        (`subtitulo` LIKE '%".$input."%')");
+
 	foreach($result->fetchAll() as $article) {
 		$list[]=new result($article['titulo'],$article['subtitulo'], $article['imagen_principal']);
 	}
 	return $list;
 }
-
-	//sleep((float)(rand(200, 2000)) / 1000);
 
 	echo json_encode(articleSearch($_POST["content"]));
 ?>
